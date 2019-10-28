404207ccc3fccccc4b0c38f39bbe86e89f8edab3
Clszz <mreynaldi908@gmail.com>
2019-10-28 14:29:36 +0700

    Delete SceneDelegate and Add Extension

---
+++ Final Challenge/Supporting Files/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Final Challenge
+//
+//  Created by Muhammad Reynaldi on 28/10/19.
+//  Copyright © 2019 12. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
