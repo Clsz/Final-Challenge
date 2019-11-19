367f86af582d0f6ad05022cb55674c8cd363e964
Steven Gunawan <stevengunawan1512@gmail.com>
2019-11-19 11:03:29 +0700

    19 Nov 11:03

---
+++ Final Challenge/Controller/Activity/Segmented View/SegmentedViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SegmentedViewController.swift
+//  Final Challenge
+//
+//  Created by Steven Gunawan on 19/11/19.
+//  Copyright © 2019 12. All rights reserved.
+//
+
+import UIKit
+
+class SegmentedViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+              setupView(text: "Progres")
+          }
+
+    
+}
---
+++ Final Challenge/Controller/Activity/Tab 2/View Second Tab/SecondTabViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondTabViewController.swift
+//  Final Challenge
+//
+//  Created by Steven Gunawan on 19/11/19.
+//  Copyright © 2019 12. All rights reserved.
+//
+
+import UIKit
+
+class SecondTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
